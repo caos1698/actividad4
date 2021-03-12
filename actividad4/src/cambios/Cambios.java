d5007aae113ceb401f686e5c660d00c18e54e3b7
Amigo <cmartinezm.al@iesjandula.es>
2021-03-12 21:40:20 +0100

    Validamos todos los pasos de la nueva rama

---
+++ actividad4/src/cambios/Cambios.java
@@ -0,0 +1,9 @@
+package cambios;
+
+public class Cambios {
+
+	public static void main(String[] args) {
+		System.out.println("Linea para los cambios");
+	}
+
+}
---
+++ actividad4/src/cambios/NuevaClase.java
@@ -0,0 +1,10 @@
+package cambios;
+
+public class NuevaClase {
+
+	public static void main(String[] args) {
+		System.out.println("Linea de prueba");
+
+	}
+
+}
